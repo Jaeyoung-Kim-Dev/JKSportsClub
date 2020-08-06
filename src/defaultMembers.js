433dc81dd154418e03febebdfd7bf1c3d76587c0
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2020-08-06 02:17:21 -0600

    second commit

---
+++ src/defaultMembers.js
@@ -0,0 +1,83 @@
+module.exports = [{
+    name: "Harry Potter",
+    email: "harry.potter@jkmail.com",
+    phone: "403-999-1234",
+    numberAdults: 2,
+    numberChild: 2,
+    createDate: "2020-06-05 13:30",
+    firstDate: "2020-08-01",
+    lastDate: "2020-08-10"
+},{
+    name: "Jason Bourne",
+    email: "jason.bourne@jkmail.com",
+    phone: "403-888-1234",
+    numberAdults: "1",
+    numberChild: "0",
+    createDate: "2020-05-10 10:30",
+    firstDate: "2020-06-01",
+    lastDate: "2020-06-15"
+},{
+    name: "Frodo Baggins",
+    email: "frodo.baggins@jkmail.com",
+    phone: "403-789-9874",
+    numberAdults: "3",
+    numberChild: "7",
+    createDate: "2020-01-19 11:30",
+    firstDate: "2020-06-30",
+    lastDate: "2020-07-15"
+},{
+    name: "Gandalf",
+    email: "gandalf@jkmail.com",
+    phone: "403-963-7654",
+    numberAdults: "2",
+    numberChild: "2",
+    createDate: "2020-03-10 10:30",
+    firstDate: "2020-12-01",
+    lastDate: "2020-12-26"
+},{
+    name: "Aragorn",
+    email: "aragorn@jkmail.com",
+    phone: "587-887-1234",
+    numberAdults: "2",
+    numberChild: "10",
+    createDate: "2020-02-01 17:45",
+    firstDate: "2020-08-17",
+    lastDate: "2020-08-20"
+},{
+    name: "Legolas",
+    email: "legolas@jkmail.com",
+    phone: "741-991-5247",
+    numberAdults: "2",
+    numberChild: "9",
+    createDate: "2020-03-10 04:30",
+    firstDate: "2020-09-14",
+    lastDate: "2020-09-15"
+},{
+    name: "Saruman",
+    email: "saruman@jkmail.com",
+    phone: "403-451-7891",
+    numberAdults: "2",
+    numberChild: "5",
+    createDate: "2020-04-05 16:18",
+    firstDate: "2020-10-02",
+    lastDate: "2020-11-15"
+},{
+    name: "Bruce Wayne",
+    email: "bruce.wayne@jkmail.com",
+    phone: "553-517-1137",
+    numberAdults: "2",
+    numberChild: "1",
+    createDate: "2020-04-30 19:21",
+    firstDate: "2020-08-29",
+    lastDate: "2020-09-10"
+},{
+    name: "Peter Parker",
+    email: "peter.parker@jkmail.com",
+    phone: "247-651-6666",
+    numberAdults: "1",
+    numberChild: "0",
+    createDate: "2020-05-10 10:30",
+    firstDate: "2020-06-01",
+    lastDate: "2020-06-15"
+}
+]
--- src/server.js
+++ src/server.js
@@ -108,11 +108,11 @@ app.get('/api/management', async (req, res) => {
     }, res);
 })
 
-app.post('/api/resetdb', async (req, res) => { //todo: 포스트맨에서는 잘되지만 웹에서는 에러.
-    withDB(async (db) => {
-        await db.collection('clubs').drop();
+app.post('/api/resetdb', (req, res) => { //todo: 포스트맨에서는 잘되지만 웹에서는 에러.
+    withDB((db) => {
+        db.collection('clubs').drop();
         db.createCollection('clubs');
-        db.collection('clubs').insert([{
+        db.collection('clubs').insertMany([{
                 initials:'baseball',
                 upvotes: 0,
                 reviews: [],
@@ -129,91 +129,91 @@ app.post('/api/resetdb', async (req, res) => { //todo: 포스트맨에서는 잘
                 upvotes: 0,
                 reviews: [],
             }]
-        )
+        );
 
-        await db.collection('members').drop();
+        db.collection('members').drop();
         db.createCollection("members");
-        db.collection('members').insert([{
-            custName: "Harry Potter",
-            custEmail: "harry.potter@jkmail.com",
-            custPhone: "403-999-1234",
-            numberAdults: 2,
-            numberChild: 2,
-            createDate: "2020-06-05 13:30",
-            firstDate: "2020-08-01",
-            lastDate: "2020-08-10"
+        db.collection('members').insertMany([{
+            name: "Harry Potter",
+            registerClub: "SOCCER",
+            DOB: "2001-01-09",
+            city: "Calgary",
+            prov: "AB",
+            phone: "403-999-1234",
+            email: "harry.potter@jkmail.com",
+            registerDate: "2020-06-05 13:30"
         },{
-            custName: "Jason Bourne",
-            custEmail: "jason.bourne@jkmail.com",
-            custPhone: "403-888-1234",
-            numberAdults: "1",
-            numberChild: "0",
-            createDate: "2020-05-10 10:30",
-            firstDate: "2020-06-01",
-            lastDate: "2020-06-15"
+            name: "Jason Bourne",
+            registerClub: "BASEBALL",
+            DOB: "2005-05-07",
+            city: "Edmonton",
+            prov: "AB",
+            phone: "403-888-1234",
+            email: "jason.bourne@jkmail.com",
+            registerDate: "2020-05-10 10:30"
         },{
-            custName: "Frodo Baggins",
-            custEmail: "frodo.baggins@jkmail.com",
-            custPhone: "403-789-9874",
-            numberAdults: "3",
-            numberChild: "7",
-            createDate: "2020-01-19 11:30",
-            firstDate: "2020-06-30",
-            lastDate: "2020-07-15"
+            name: "Frodo Baggins",
+            registerClub: "SOCCER",
+            DOB: "2003-12-21",
+            city: "Vancouver",
+            prov: "BC",
+            phone: "403-789-9874",
+            email: "frodo.baggins@jkmail.com",
+            registerDate: "2020-01-19 11:30"
         },{
-            custName: "Gandalf",
-            custEmail: "gandalf@jkmail.com",
-            custPhone: "403-963-7654",
-            numberAdults: "2",
-            numberChild: "2",
-            createDate: "2020-03-10 10:30",
-            firstDate: "2020-12-01",
-            lastDate: "2020-12-26"
+            name: "Gandalf",
+            registerClub: "ICE HOCKEY",
+            DOB: "2004-10-31",
+            city: "Toronto",
+            prov: "ON",
+            phone: "403-963-7654",
+            email: "gandalf@jkmail.com",
+            registerDate: "2020-03-10 10:30"
         },{
-            custName: "Aragorn",
-            custEmail: "aragorn@jkmail.com",
-            custPhone: "587-887-1234",
-            numberAdults: "2",
-            numberChild: "10",
-            createDate: "2020-02-01 17:45",
-            firstDate: "2020-08-17",
-            lastDate: "2020-08-20"
+            name: "Aragorn",
+            registerClub: "BASKETBALL",
+            DOB: "2006-07-20",
+            city: "Calgary",
+            prov: "AB",
+            phone: "587-887-1234",
+            email: "aragorn@jkmail.com",
+            registerDate: "2020-02-01 17:45"
         },{
-            custName: "Legolas",
-            custEmail: "legolas@jkmail.com",
-            custPhone: "741-991-5247",
-            numberAdults: "2",
-            numberChild: "9",
-            createDate: "2020-03-10 04:30",
-            firstDate: "2020-09-14",
-            lastDate: "2020-09-15"
+            name: "Legolas",
+            registerClub: "SOCCER",
+            DOB: "2006-06-11",
+            city: "Quebec City",
+            prov: "QC",
+            phone: "741-991-5247",
+            email: "legolas@jkmail.com",
+            registerDate: "2020-03-10 04:30"
         },{
-            custName: "Saruman",
-            custEmail: "saruman@jkmail.com",
-            custPhone: "403-451-7891",
-            numberAdults: "2",
-            numberChild: "5",
-            createDate: "2020-04-05 16:18",
-            firstDate: "2020-10-02",
-            lastDate: "2020-11-15"
+            name: "Saruman",
+            registerClub: "BASEBALL",
+            DOB: "2009-10-11",
+            city: "Ottawa",
+            prov: "ON",
+            phone: "403-451-7891",
+            email: "saruman@jkmail.com",
+            registerDate: "2020-04-05 16:18"
         },{
-            custName: "Bruce Wayne",
-            custEmail: "bruce.wayne@jkmail.com",
-            custPhone: "553-517-1137",
-            numberAdults: "2",
-            numberChild: "1",
-            createDate: "2020-04-30 19:21",
-            firstDate: "2020-08-29",
-            lastDate: "2020-09-10"
+            name: "Bruce Wayne",
+            registerClub: "BASEBALL",
+            DOB: "2009-03-04",
+            city: "Montreal",
+            prov: "QC",
+            phone: "553-517-1137",
+            email: "bruce.wayne@jkmail.com",
+            registerDate: "2020-04-30 19:21"
         },{
-            custName: "Peter Parker",
-            custEmail: "peter.parker@jkmail.com",
-            custPhone: "247-651-6666",
-            numberAdults: "1",
-            numberChild: "0",
-            createDate: "2020-05-10 10:30",
-            firstDate: "2020-06-01",
-            lastDate: "2020-06-15"
+            name: "Peter Parker",
+            registerClub: "BASKETBALL",
+            DOB: "2005-11-07",
+            city: "Winnipeg",
+            prov: "MB",
+            phone: "247-651-6666",
+            email: "peter.parker@jkmail.com",
+            registerDate: "2020-05-10 10:30"
         }
         ]);
 
