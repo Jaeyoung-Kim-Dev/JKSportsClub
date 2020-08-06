71560c33ec2bd3c63d929d46b0f81bdba57fdc1e
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2020-08-06 02:17:21 -0600

    second commit

---
+++ src/defaultClubs.js
@@ -0,0 +1,17 @@
+module.exports = [{
+    initials:'baseball',
+    upvotes: 0,
+    reviews: [],
+},{
+    initials:'soccer',
+    upvotes: 0,
+    reviews: [],
+},{
+    initials:'basketball',
+    upvotes: 0,
+    reviews: [],
+},{
+    initials:'icehockey',
+    upvotes: 0,
+    reviews: [],
+}]
