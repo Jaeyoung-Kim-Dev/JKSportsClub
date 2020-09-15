a2fcc117b21cf7d86f1f4bf0221d121f69dbc2ff
Jaeyoung Kim <50346571+Jaeyoung-Kim-CND@users.noreply.github.com>
2020-09-14 23:01:46 -0600

    Build front app and copied it to the backend

---
+++ server/build/precache-manifest.1e6a976a3b8b63dbfef0bbb010254239.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a5c7e6b7de436cd9a10ae936485190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da4c9860a182a7675a5",
+    "url": "/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "35e7fc8a5bc1570d522c",
+    "url": "/static/css/main.2387e989.chunk.css"
+  },
+  {
+    "revision": "6da4c9860a182a7675a5",
+    "url": "/static/js/2.0424ed69.chunk.js"
+  },
+  {
+    "revision": "3b9c7371001b529e29ee64127687311a",
+    "url": "/static/js/2.0424ed69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e7fc8a5bc1570d522c",
+    "url": "/static/js/main.64bf5a4b.chunk.js"
+  },
+  {
+    "revision": "71d27fe47fbfe6adf020",
+    "url": "/static/js/runtime-main.cbaa4a02.js"
+  },
+  {
+    "revision": "94b59f3343cf48f31fe01c0f4b35e48f",
+    "url": "/static/media/baseball.94b59f33.jpg"
+  },
+  {
+    "revision": "048acb5f974420787b8e5ab177f9c6c2",
+    "url": "/static/media/basketball.048acb5f.jpg"
+  },
+  {
+    "revision": "b6f3cbc62c303ff447e76b4a6ba83c95",
+    "url": "/static/media/icehockey.b6f3cbc6.jpg"
+  },
+  {
+    "revision": "16125cb8872e09e1d6e039018bf3070f",
+    "url": "/static/media/logo.16125cb8.jpg"
+  },
+  {
+    "revision": "fd54ef4440f30d81f090d15bf29b3aa1",
+    "url": "/static/media/soccer.fd54ef44.jpg"
+  }
+]);
